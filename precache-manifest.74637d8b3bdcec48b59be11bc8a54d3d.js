0cefbd8fb69a8e1675877255de4786c1b3a2fd6b
Wesley Kater <wesley.kater@icloud.com>
2020-07-30 21:07:37 +0200

    Updates

---
+++ precache-manifest.74637d8b3bdcec48b59be11bc8a54d3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05d0be4e029f5557de9e2ef56c9619e",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "f512c89999d28e67238c",
+    "url": "/Portfolio-Website/static/css/main.f9b5bbe9.chunk.css"
+  },
+  {
+    "revision": "d24958af669b47c22c44",
+    "url": "/Portfolio-Website/static/js/2.e71da0c3.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Portfolio-Website/static/js/2.e71da0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f512c89999d28e67238c",
+    "url": "/Portfolio-Website/static/js/main.37f33d4b.chunk.js"
+  },
+  {
+    "revision": "dde95ee7596232cc0630",
+    "url": "/Portfolio-Website/static/js/runtime-main.f0738ad6.js"
+  },
+  {
+    "revision": "8bd7fa6673ccd6c90e2fd93a83b7884f",
+    "url": "/Portfolio-Website/static/media/profile.8bd7fa66.jpeg"
+  }
+]);
